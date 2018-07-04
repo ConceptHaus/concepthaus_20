712f9612e9eed1039d24bc92572fc59e8054b120
Sergio Ramos <sergirams@gmail.com>
2018-07-03 19:41:54 -0500

    cambios

--- resources/views/home/socialads.blade.php
+++ resources/views/home/socialads.blade.php
@@ -133,34 +133,34 @@
       <img class="img-icon-title" src="{{asset('img/conceptRight.svg')}}" alt="ConceptHaus"> Clientes
     </h2>
     <div class="row text-center align-items-center">
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/citibanamex.svg')}}" class="client-brand brand1" alt="Concept Haus / Citibanamex">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/myst.svg')}}" class="client-brand brand2" alt="Concept Haus / Baco">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/soulcore.svg')}}" class="client-brand brand3" alt="Concept Haus / Soulcore">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/tinkerlink.png')}}" class="client-brand brand4" alt="Concept Haus / Fox">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/nationalgeo.svg')}}" class="client-brand brand5" alt="Concept Haus / National Geography">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/origenes.svg')}}" class="client-brand brand6" alt="Concept Haus / The Walking Dead">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/salvajetentacion.svg')}}" class="client-brand brand7" alt="Concept Haus / Fox Sports">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/draft.svg')}}" class="client-brand brand8" alt="Concept Haus / Discovery">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/totalplay.svg')}}" class="client-brand brand11" alt="Concept Haus / Clarasol">
       </div>
-      <div class="col-sm-6 col-md-2">
+      <div class="col-sm-6 col-md-3">
         <img src="{{asset('img/clients/chilimbalam.svg')}}" class="client-brand brand12" alt="Concept Haus / Chilim Balam">
       </div>
     </div>
