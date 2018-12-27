71d8857285e50765ce7a8b29f90b10622f17fcd0
fcastaneda9725 <f.castanedatrujillo@gmail.com>
2018-12-27 09:38:23 -0600

    closes #2

--- resources/views/home/project.blade.php
+++ resources/views/home/project.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    {{-- <div class="row" style="margin-top: 10%;">
-        <div class="col-md-12">
-            <h1 class="text-center">Proyecto</h1>
-            <!-- <p>{{$details}}</p> -->
-
-             <!-- @foreach ($details as $prueba)
-                <p>{{ $prueba }}</p>
-            @endforeach  -->
-
-        </div>
-    </div> --}}
     <div class="d-flex" style="margin-top:10%;" ng-controller="WelcomeController">
       <div class="container" ng-init="getOneProject({{$id}})" ng-cloak>
         <div class="nombre row row-flex row-flex-wrap c-gray">
