ff5a42ad31fa72434ea2c544ffb88e08dd64c9aa
LeslyePO <79282745+LeslyePO@users.noreply.github.com>
2021-08-16 13:37:56 -0500

    Update welcome.blade.php

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -99,9 +99,9 @@
         </div>
       </div>
 
-      <!-- Discovery Kids && Sistema Eco -->
+      <!-- Discovery Kids -->
       <div class="col-md-3 w25 project-animate-2">
-        <div class="containerProject project-effect" ng-repeat="project in projects" ng-if="project.id == '61179187' || project.id == '56727549'">
+        <div class="containerProject project-effect" ng-repeat="project in projects" ng-if="project.id == '61179187'">
             <a class="projectName" href="{{url('proyecto/<%project.id%>')}}" target="_self">
               <figure class="effect-goliath">
                 <img ng-src="<% project.covers.original %>"/>
@@ -115,6 +115,25 @@
             </a>
         </div>
       </div>
+
+      <!-- Discovery Kids && Sistema Eco -->
+     <!-- <div class="col-md-3 w25 project-animate-2">
+        <div class="containerProject project-effect" ng-repeat="project in projects" ng-if="project.id == '61179187' || project.id == '56727549'">
+            <a class="projectName" href="{{url('proyecto/<%project.id%>')}}" target="_self">
+              <figure class="effect-goliath">
+                <img ng-src="<% project.covers.original %>"/>
+                <figcaption>
+                  <p class="projectCliente">
+                    <span class="title"><% project.name %></span> <br>
+                    <span ng-repeat="field in project.fields"><% field %></span>
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+        </div>
+      </div>-->
+      
+      
       <!-- Clarasol && Fox/Latinoamérica -->
       <div class="col-md-3 w25 project-animate-3">
         <div class="containerProject project-effect" ng-repeat="project in projects" ng-if="project.id == '55916489' || project.id == '56050953'">
