0b4731f48356c0189960f90aaac1c0ba2fe93cf7
Víctor Martínez <victor@MacBook-Air-de-99.local>
2023-12-20 14:21:51 -0600

    Add google pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,13 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
+    <!-- Google Tag Manager Diciembre 2023-->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TKWP5GV4');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <!-- Meta Tags -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" / <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,16 +50,21 @@
     <link rel="stylesheet" href="/css/bootstrap-select.min.css"> --}}
     {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" /> --}}
 
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <!-- Google Tag Manager Old-->
+    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TLGCBXH');</script>
+    })(window,document,'script','dataLayer','GTM-TLGCBXH');</script> -->
     <!-- End Google Tag Manager -->
 </head>
 
 <body ng-app="app" class="body_prim">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKWP5GV4"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <div class="center">
         <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>
